cf58aa60c715c174285df6957ff811960b8cc081
sinethemba <poni.sneh@gmail.com>
2020-07-24 10:12:49 +0200

    Scala assignment

--- src/Course.scala
+++ src/Course.scala
@@ -1,4 +1,15 @@
 object Course {
+    def main (args: Array [String]): Unit ={
+
+        case class Course (courseName: String, coursePrice: Double, body: String)
+
+        val course1 = Course (implicit  "ICE362S Elective, FT,", "Course Price : R "+ 40.000.hashCode())
+        val course2 = Course (implicit  courseName = "ICE262S Elective, FT", coursePrice = "Course Price :R" +35.000.hashCode())
+        val course3 = Course (implicit courseName = "- ICF152S Elective, FT", coursePrice = "Course Price: R"+ 30.000.hashCode() )
+
+        println(course1,course2,course3)
+
+    }
 
 
 }
